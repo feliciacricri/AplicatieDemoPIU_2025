829f6dad2606f4a98eae1fb78e747b47c05cdaa5
Felicia Giza <felicia@usm.ro>
2024-04-14 22:01:42 +0300

    afisare informatii despre studenti in formular Windows

--- EvidentaStudenti/EvidentaStudenti_UI_WindowsForms/Form1.Designer.cs
+++ EvidentaStudenti/EvidentaStudenti_UI_WindowsForms/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Text = "Form1";
         }
 
--- EvidentaStudenti/EvidentaStudenti_UI_WindowsForms/Form1.cs
+++ EvidentaStudenti/EvidentaStudenti_UI_WindowsForms/Form1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using LibrarieModele;
+using NivelStocareDate;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,108 @@ namespace EvidentaStudenti_UI_WindowsForms
 {
     public partial class Form1 : Form
     {
+        AdministrareStudenti_FisierText adminStudenti;
+
+        private Label lblNume;
+        private Label lblPrenume;
+        private Label lblNote;
+
+        private Label[] lblsNume;
+        private Label[] lblsPrenume;
+        private Label[] lblsNote;
+
+        private const int LATIME_CONTROL = 100;
+        private const int DIMENSIUNE_PAS_Y = 30;
+        private const int DIMENSIUNE_PAS_X = 120;
+
         public Form1()
         {
             InitializeComponent();
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            string locatieFisierSolutie = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            // setare locatie fisier in directorul corespunzator solutiei
+            // astfel incat datele din fisier sa poata fi utilizate si de alte proiecte
+            string caleCompletaFisier = locatieFisierSolutie + "\\" + numeFisier;
+
+            adminStudenti = new AdministrareStudenti_FisierText(caleCompletaFisier);
+            int nrStudenti = 0;
+
+            Student[] studenti = adminStudenti.GetStudenti(out nrStudenti);
+
+            //setare proprietati
+            this.Size = new Size(500, 200);
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(100, 100);
+            this.Font = new Font("Arial", 9, FontStyle.Bold);
+            this.ForeColor = Color.LimeGreen;
+            this.Text = "Informatii studenti";
+
+            //adaugare control de tip Label pentru 'Nume';
+            lblNume = new Label();
+            lblNume.Width = LATIME_CONTROL;
+            lblNume.Text = "Nume";
+            lblNume.Left = DIMENSIUNE_PAS_X;
+            lblNume.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblNume);
+
+            //adaugare control de tip Label pentru 'Prenume';
+            lblPrenume = new Label();
+            lblPrenume.Width = LATIME_CONTROL;
+            lblPrenume.Text = "Prenume";
+            lblPrenume.Left = 2 * DIMENSIUNE_PAS_X;
+            lblPrenume.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblPrenume);
+
+            //adaugare control de tip Label pentru 'Note';
+            lblNote = new Label();
+            lblNote.Width = LATIME_CONTROL;
+            lblNote.Text = "Note";
+            lblNote.Left = 3 * DIMENSIUNE_PAS_X;
+            lblNote.ForeColor = Color.DarkGreen;
+            this.Controls.Add(lblNote);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AfiseazaStudenti();
+        }
+
+        private void AfiseazaStudenti()
+        {
+            Student[] studenti = adminStudenti.GetStudenti(out int nrStudenti);
+
+            lblsNume = new Label[nrStudenti];
+            lblsPrenume = new Label[nrStudenti];
+            lblsNote = new Label[nrStudenti];
+
+            int i = 0;
+            foreach (Student student in studenti)
+            {
+                //adaugare control de tip Label pentru numele studentilor;
+                lblsNume[i] = new Label();
+                lblsNume[i].Width = LATIME_CONTROL;
+                lblsNume[i].Text = student.Nume;
+                lblsNume[i].Left = DIMENSIUNE_PAS_X;
+                lblsNume[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblsNume[i]);
+
+                //adaugare control de tip Label pentru prenumele studentilor
+                lblsPrenume[i] = new Label();
+                lblsPrenume[i].Width = LATIME_CONTROL;
+                lblsPrenume[i].Text = student.Prenume;
+                lblsPrenume[i].Left = 2 * DIMENSIUNE_PAS_X;
+                lblsPrenume[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblsPrenume[i]);
+
+                //adaugare control de tip Label pentru notele studentilor
+                lblsNote[i] = new Label();
+                lblsNote[i].Width = LATIME_CONTROL;
+                lblsNote[i].Text = string.Join(" ", student.GetNote());
+                lblsNote[i].Left = 3 * DIMENSIUNE_PAS_X;
+                lblsNote[i].Top = (i + 1) * DIMENSIUNE_PAS_Y;
+                this.Controls.Add(lblsNote[i]);
+                i++;
+            }
         }
     }
 }
